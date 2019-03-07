7201e69f305874af96279b5f62a02c63bb9469e5
bjgribb <bjgribb@gmail.com>
2019-03-07 14:36:58 -0500

    almost thru part 8

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns 
 import uuid # Required for unique book instances
+from django.contrib.auth.models import User
+from datetime import date
 # Create your models here.
 
 class Genre(models.Model):
@@ -20,7 +22,7 @@ class Book(models.Model):
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
 
-    def __str__(sefl):
+    def __str__(self):
         """String for representing the Model object."""
         return self.title
 
@@ -28,12 +30,19 @@ class Book(models.Model):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_genre(self):
+        """create a string for the Genre, handles the many-to-many database cost of displaying admin"""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -56,6 +65,12 @@ class BookInstance(models.Model):
     def __str__(self):
         return f'{self.id} ({self.book.title})'
 
+    @property
+    def is_overdue(selfK):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
 class Author(models.Model):
     """Model representing an author."""
     first_name = models.CharField(max_length=100)
@@ -71,4 +86,13 @@ class Author(models.Model):
 
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
+
+
+class Language(models.Model):
+    """Model to represent language of book"""
+    name = models.CharField(max_length=255, help_text="Enter the language the book is written in.")
+
+    def __str__(self):
+        return self.name
+
 
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
-
+from catalog.models import Book, Author, BookInstance, Genre, Language
+from django.views import generic
 # Create your views here.
+
+def index(request):
+    """view function for the home page of site"""
+
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    #Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    #the 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
